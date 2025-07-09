f538be2332d97f66e5fae1f6c8d4becfc892923e
Mr-Shubham-Palde <shubhampalde96@gmail.com>
2025-07-09 17:02:09 +0530

    until episode 4 done

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 
 const app = express();
-// why we use app.use()?
-// app.use() is used to mount middleware functions at a specific path.
-
-app.use('/user',(req,res)=>{
-    //route handler for /user
-    res.send("Route Handler for  1")
+app.use("/about",(req,res)=>{
+    res.send("This is about route handler");
 })
+app.use("/about/contact",(req,res)=>{
+    res.send("This is contact route handler");
+}       )
+// app.use("/",(req,res)=>{
+//     console.log("This is my first request");
+//     res.send("Hello World")
+// })
+app.get("/user",(req,res)=>{
+    res.send("This is user route handler")
+})
+
 
 
 
