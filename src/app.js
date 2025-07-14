19239dd5bd20557f8660dd981ec10f8b2c5fb2c2
Mr-Shubham-Palde <shubhampalde96@gmail.com>
2025-07-14 21:58:53 +0530

    Done the API for the update and delete

--- src/app.js
+++ src/app.js
@@ -27,6 +27,24 @@ app.post("/signup",async(req,res)=>{
 
 })
 
+//get user by email id
+app.get("/user",async(req,res)=>{
+    const email = req.body.emailid
+    try {
+
+        const Users = await User.findOne({emailid:email})
+        if (Users.length === 0){
+            res.status(400).send("User Not Exists")
+        }
+        else{
+            res.send(Users)
+        }
+        
+    } catch (error) {
+        res.status(400).send("User Not fnd")
+    }})
+
+
 //write an api to get the data of all the users
 app.get("/feed",async(req,res)=>{{
     try {
@@ -39,6 +57,33 @@ app.get("/feed",async(req,res)=>{{
     }
 }})
 
+app.delete("/deleteuser",async(req,res)=>{
+    const userid = req.body.userid;
+    try {
+        const user = await User.findByIdAndDelete(userid);
+        res.send("User deleted successfully")
+    } catch (error) {
+        res.status(400).send("User not found or cnnot be deleted")
+        
+    }
+})
+
+//update the data of the user
+
+app.patch("/update",async(req,res)=>{
+
+    const userid = req.body.userid;
+    const data = req.body;
+    try {
+
+        const user = await User.findByIdAndUpdate({_id:userid},data)
+        res.send("User Updated Successfully")
+        
+    } catch (error) {
+        res.status(400).send("User not found or cannot be updated")
+    }
+
+})
 
 connectDB().then(()=>{
     console.log("Connection established with mongodb")
